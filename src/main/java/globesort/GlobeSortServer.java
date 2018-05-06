0d3c0044de933ff352b75bcf1178f20dceb97856
Kyle Du <jid020@ucsd.edu>
2018-05-05 23:35:06 -0700

    hello

--- src/main/java/globesort/GlobeSortServer.java
+++ src/main/java/globesort/GlobeSortServer.java
@@ -96,7 +96,7 @@ public class GlobeSortServer {
             IntArray response = responseBuilder.build();
             long endTime   = System.nanoTime();
             long totalTime = endTime - startTime;
-            System.out.println("######Total time is: " + totalTime/1000000000);
+            System.out.println("######Total time (ns) is: " + totalTime);
             responseObserver.onNext(response);
             responseObserver.onCompleted();
         }
